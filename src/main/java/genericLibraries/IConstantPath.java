7c267c3d5533fffefe4dccc99f856698c7dda32d
visib <visib@Master>
2023-11-08 11:19:29 +0530

    commit

---
+++ src/main/java/genericLibraries/IConstantPath.java
@@ -0,0 +1,12 @@
+package genericLibraries;
+
+/**
+ * This interface contains all the external file paths
+ * @author Rahul
+ */
+public interface IConstantPath {
+
+	String PROPERTIES_PATH="./src/test/resources/commondata.properties";
+	String EXCEL_PATH="./src/test/resources/SkillRary TestData.xlsx";
+	
+}
